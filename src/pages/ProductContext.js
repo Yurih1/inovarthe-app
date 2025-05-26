8601bb703da73fd6b3a5f265020d2cf18fdce9a9
Yuri O. Santos <yurih2009@gmail.com>
2025-05-25 21:57:15 -0300

    Update ProductContext.js

--- src/pages/ProductContext.js
+++ src/pages/ProductContext.js
@@ -1,28 +1,43 @@
 import React, { createContext, useState, useEffect } from 'react';
+import { fetchProducts } from '../services/api'; 
 
 export const ProductContext = createContext();
 
 export const ProductProvider = ({ children }) => {
   const [allProducts, setAllProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
-    const storedProducts = localStorage.getItem('randomProducts');
-    if (storedProducts) {
-      setAllProducts(JSON.parse(storedProducts));
-    }
+    const loadProducts = async () => {
+      const products = await fetchProducts();
+      setAllProducts(products);
+      setFilteredProducts(products); 
+    };
+
+    loadProducts();
   }, []);
 
-  // Filtra os produtos conforme o texto buscado
-  const filteredProducts = searchQuery
-    ? allProducts.filter((product) =>
+  useEffect(() => {
+    if (searchQuery) {
+      const filtered = allProducts.filter((product) =>
         product.Nome.toLowerCase().includes(searchQuery.toLowerCase())
-      )
-    : allProducts;
+      );
+      setFilteredProducts(filtered);
+    } else {
+      setFilteredProducts(allProducts);
+    }
+  }, [searchQuery, allProducts]);
 
   return (
     <ProductContext.Provider
-      value={{ allProducts, searchQuery, setSearchQuery, filteredProducts }}
+      value={{
+        allProducts,
+        filteredProducts,
+        setFilteredProducts,
+        searchQuery,
+        setSearchQuery,
+      }}
     >
       {children}
     </ProductContext.Provider>
